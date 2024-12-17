a823bd879b0524ed06243e3cacd7a2816409f98c
Max Ivanov <ima20070111@gmail.com>
2024-12-17 21:46:54 +0200

    1.3

---
+++ front/components/header.js
@@ -0,0 +1,54 @@
+"use client";
+
+import Link from 'next/link';
+import Image from 'next/image';
+import '$style/Header.css'
+import { usePathname } from 'next/navigation';
+
+const Header = () => {
+
+	const location = usePathname();
+	console.log(location);
+
+	return (
+		<header className="header">
+			<div className="header__container">
+				<div className="left">
+					<Link href="/info">
+						<div className="logo_shveya">
+							<Image
+								src="/images/logo.png"
+								alt="Logo"
+								width={196}
+								height={61}
+								layout="intrinsic"
+								className="logo-img"
+								priority
+							/>
+						</div>
+					</Link>
+				</div>
+
+				<button className="icon-menu">
+					<span></span>
+				</button>
+
+				<div className="right _menu">
+					<nav className="menu">
+						{location == '/info'
+							? <Link className='menu__link' href="/guides">Навчальний центр</Link>
+							: <Link className='menu__link' href="/info">Головна сторінка</Link>
+						}
+					</nav>
+					<div className="menulang">
+						<button>EN</button>
+						<div className="_line"></div>
+						<button>UK</button>
+					</div>
+				</div>
+			</div>
+		</header>
+	);
+};
+
+export default Header;
--- front/src/app/layout.js
+++ front/src/app/layout.js
@@ -1,28 +1,29 @@
 import '$style/globals.css'
-
+import Header from '$component/Header'
 
 export default function RootLayout({ children }) {
 
-   return (
+  return (
 
-     <html lang="en">
+    <html lang="en">
 
-       <head>
-         <title>Horizon</title>
-         <meta name="description" content="Platform for maximum effective language learning" />
-         <meta charSet="UTF-8" />
-         <meta name="keywords" content="language learning, education, platform" />
-         <meta name="author" content="Blossom team" />
-         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-         <meta name="robots" content="index, follow" />
-       </head>
+      <head>
+        <title>Horizon</title>
+        <meta name="description" content="Platform for maximum effective language learning" />
+        <meta charSet="UTF-8" />
+        <meta name="keywords" content="language learning, education, platform" />
+        <meta name="author" content="Blossom team" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="robots" content="index, follow" />
+      </head>
 
-       <body>
-         <main>{children}</main>
-       </body>
+      <body>
+        <div className='wrapper'>
+          <Header></Header>
+          <main>{children}</main>
+        </div>
+      </body>
+    </html>
 
-     </html>
-
-   );
- }
- 
+  );
+}
