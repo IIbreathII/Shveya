3fc35446424a3cede4f5ff9f5b13bcb3294f0f1f
IIbreathII <96979747+IIbreathII@users.noreply.github.com>
2024-12-17 19:08:05 +0200

    secound

--- front/src/app/page.js
+++ front/src/app/page.js
@@ -10,9 +10,11 @@ export default function HomePage() {
   };
 
   return (
+
     <div className="bg-blue-500 text-white p-4">
       <h1 className="text-2xl font-bold">Страница входа</h1>
       <button onClick={navigateToLogin}>Go to Login</button>
     </div>
+    
   );
 }
