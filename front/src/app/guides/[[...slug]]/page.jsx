9e77096eb10178dfe3badfdc4bf98115a2ffc42f
REZNYA1 <m.vaznenkov@gmail.com>
2025-02-09 20:05:20 +0200

    2.1 guides

--- front/src/app/guides/[[...slug]]/page.jsx
+++ front/src/app/guides/[[...slug]]/page.jsx
@@ -1,26 +1,24 @@
-"use client"; // Если используются клиентские функции
+"use client";
 
-import { useEffect, useRef, useState } from 'react';
-import '$style/Guides.css'
-import Aside from '$component/guides/Aside/Aside';
-import MasterClassSect from '$component/guides/MasterClassSect/MasterClassSect';
-import VideoSect from '$component/guides/VideoSect/VideoSect';
-import ExamplesSect from '$component/guides/ExamplesSect/ExamplesSect';
-import DetailsSect from '$component/guides/DetailsSect/DetailsSect';
-import AuthorSect from '$component/guides/AuthorSect/AuthorSect';
-import ComSect from '$component/guides/ComSect/ComSect';
-import Questions from '$component/guides/Questions/Questions';
-import { useParams } from 'next/navigation';
-import { getData } from 'api';
+import { useEffect, useRef, useState } from "react";
+import "$style/Guides.css";
+import Aside from "$component/guides/Aside/Aside";
+import MasterClassSect from "$component/guides/MasterClassSect/MasterClassSect";
+import VideoSect from "$component/guides/VideoSect/VideoSect";
+import ExamplesSect from "$component/guides/ExamplesSect/ExamplesSect";
+import DetailsSect from "$component/guides/DetailsSect/DetailsSect";
+import AuthorSect from "$component/guides/AuthorSect/AuthorSect";
+import Questions from "$component/guides/Questions/Questions";
+import { useParams } from "next/navigation";
+import { getData } from "api";
 
 export default function MasterClassPage() {
-
   const [categories, setCategories] = useState([]);
   const [guidesData, setGuidesData] = useState({});
   const firstCategory = useRef();
 
   const params = useParams();
-  const { slug } = params
+  const { slug } = params;
 
   useEffect(() => {
     getData("categories", setCategories);
@@ -41,33 +39,30 @@ export default function MasterClassPage() {
     }
   }, [categories, slug]);
 
-  useEffect(e => {
-    console.log(getData);
-  }, [guidesData])
-
   return (
-    <main className='main'>
-      <div className="content">
-        <Questions />
-        {/* Ліва колонка */}
-        <Aside categories={categories} />
-        {/* Основний контент */}
-        <main className="main-content">
-          {guidesData.authors ? (
-            <>
-              <MasterClassSect masterClassData={guidesData} />
-              <VideoSect masterClassData={guidesData} />
-              <ExamplesSect masterClassData={guidesData} />
-              <DetailsSect masterClassData={guidesData} />
-              <AuthorSect masterClassData={guidesData} />
-            </>
-          ) : (
-            <div className="spinner-border" role="status">
-              <span className="visually-hidden">Loading...</span>
-            </div>
-          )}
-        </main>
+      
+      <div className="wrapper">
+      <Questions />
+      <Aside categories={categories} />
+        <div className="container">
+          
+          <main className="main-content">
+            {guidesData.authors ? (
+              <>
+                <MasterClassSect masterClassData={guidesData} />
+                <VideoSect masterClassData={guidesData} />
+                <ExamplesSect masterClassData={guidesData} />
+                <DetailsSect masterClassData={guidesData} />
+                <AuthorSect masterClassData={guidesData} />
+              </>
+            ) : (
+              <div className="spinner-border" role="status">
+                <span className="visually-hidden">Loading...</span>
+              </div>
+            )}
+          </main>
+        </div>
       </div>
-    </main>
+
   );
 }
