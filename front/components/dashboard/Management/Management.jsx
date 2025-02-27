5e0fdd4f327dc52a7bd07ee1dcd3f1586bbfb5f7
IIbreathII <96979747+IIbreathII@users.noreply.github.com>
2025-02-28 01:49:01 +0200

    9.2.0 Plot

--- front/components/dashboard/Management/Management.jsx
+++ front/components/dashboard/Management/Management.jsx
@@ -16,12 +16,14 @@ const Management = () => {
 			<div className="mt-3">
 				<ul className="list-group">
 					<ManagementElement title="Слайди" number={statistic.Slide} addLink="dashboard/slides/add" link="dashboard/slides" />
+					<ManagementElement title="Cлайди-сюжети" number={statistic.Plot} addLink="dashboard/plots/add" link="dashboard/plots" />
 					<ManagementElement title="Статистичні картки" number={statistic.card} addLink="dashboard/cards/add" link="dashboard/cards" />
 					<ManagementElement title="Медіа посилання" number={statistic.MediaLinks} addLink="dashboard/media/add" link="dashboard/media" />
 					<ManagementElement title="Партнери" number={statistic.Partners} addLink="dashboard/partners/add" link="dashboard/partners" />
 					<ManagementElement title="Маркери на мапі" number={statistic.Marker} addLink="dashboard/markers/add" link="dashboard/markers" />
 					<ManagementElement title="Категорії кібер-одягу" number={statistic.Category} addLink="dashboard/categories/add" link="dashboard/categories" />
 					<ManagementElement title="Майстер-класи" number={statistic.Subcategory} addLink="dashboard/masterclass/add" link="dashboard/masterclass" />
+					
 				</ul>
 			</div>
 		</>
